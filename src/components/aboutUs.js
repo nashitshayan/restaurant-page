b9fa060a2f1937d37900bc278652d34968bef993
Nashit Shayan <shayan.nashit@gmail.com>
2022-04-13 17:12:24 +0530

    new build

--- src/components/aboutUs.js
+++ src/components/aboutUs.js
@@ -1,25 +1,22 @@
 import chefImgURL from '../images/chef.svg';
 
-export default function aboutUs(){
-    
-    //aboutUs div
-    const aboutUsContent= document.createElement('div');
-    aboutUsContent.id ='aboutUsContent';
-    aboutUsContent.classList.add('tabContent');
+export default function aboutUs() {
+	//aboutUs div
+	const aboutUsContent = document.createElement('div');
+	aboutUsContent.id = 'aboutUsContent';
+	aboutUsContent.classList.add('tabContent');
 
+	// chef img
+	const imgDiv = document.createElement('div');
+	const chefImg = new Image();
+	chefImg.src = chefImgURL;
+	chefImg.id = 'chefImg';
+	imgDiv.appendChild(chefImg);
 
-    // chef img
-    const imgDiv= document.createElement('div');
-    const chefImg = new Image();
-    chefImg.src= chefImgURL;
-    chefImg.id= 'chefImg';
-    imgDiv.appendChild(chefImg);
-    
-
-    //about Chef Div
-    const aboutChef = document.createElement('div');
-    aboutChef.id='aboutChef';
-    aboutChef.innerHTML= `
+	//about Chef Div
+	const aboutChef = document.createElement('div');
+	aboutChef.id = 'aboutChef';
+	aboutChef.innerHTML = `
         <div>
             <h2>Our Story</h2>
             <p>Based locally in City, we serve fresh home-cooked halal food.  Special care is taken to make sure all ingredients are fresh.</p>
@@ -30,10 +27,10 @@ export default function aboutUs(){
             <h2>Our Vision</h2>
             <p>We believe in spreading warmth and happiness through our services. Currently we're a small business run by a family, but we work hard and wish to expand our business to serve more people.</p>
         </div>
-    `
+    `;
 
-    //add imgDiv and aboutChef div to aboutUsContent div
-    aboutUsContent.append(imgDiv, aboutChef);
+	//add imgDiv and aboutChef div to aboutUsContent div
+	aboutUsContent.append(imgDiv, aboutChef);
 
-  return aboutUsContent;
+	return aboutUsContent;
 }
